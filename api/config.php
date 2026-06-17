b18e3d772ef840fa5fd8ba0312e5f89cc2e71db5
Jack13-Han <hanwaihtun1e.com>
2026-06-17 21:11:35 +0900

    Update CORS headers to dynamically allow all origins for production compatibility

--- api/config.php
+++ api/config.php
@@ -1,16 +1,10 @@
 <?php
 
-$allowedOrigins = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-];
-
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
-
-if (in_array($origin, $allowedOrigins, true)) {
+if (!empty($origin)) {
     header("Access-Control-Allow-Origin: $origin");
 } else {
-    header("Access-Control-Allow-Origin: http://localhost:3000");
+    header("Access-Control-Allow-Origin: *");
 }
 
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
